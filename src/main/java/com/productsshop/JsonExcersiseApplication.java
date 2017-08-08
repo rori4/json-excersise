9df717ee7eb51274cd28e4d33ba3ef4d54aa02d4
Rangel <RangelStoilov@students.softuni.bg>
2017-08-08 15:27:20 +0300

    Initial commit

---
+++ src/main/java/com/productsshop/JsonExcersiseApplication.java
@@ -0,0 +1,12 @@
+package com.productsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonExcersiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonExcersiseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/productsshop/JsonExcersiseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.productsshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JsonExcersiseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
